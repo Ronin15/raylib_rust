dabd4bf97a8bc9062e6db883c9736e4b08a6e1c3
John <simsjohn15@gmail.com>
2024-12-09 16:02:01 -0800

    working ratlib examples

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
+
 use raylib::prelude::*;
+const TARGET_FPS:u32 = 60;
 
 fn main() {
+
     let (mut rl, thread) = raylib::init()
-        .size(640, 480)
+        .size(1920, 1080)
         .title("Hello, World")
+        .vsync()
         .build();
 
-    while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
+       rl.set_target_fps(TARGET_FPS);
+       let player = Vector2::new(500.0,500.0);
+       
+       let tex = rl.load_texture(&thread,"assests/img/hammerForge.png").unwrap();
 
-        d.clear_background(Color::WHITE);
-        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+    while !rl.window_should_close() {
+        let mut r = rl.begin_drawing(&thread);
+        r.draw_fps(100, 100);
+        r.draw_texture_v(&tex, &player, Color::WHITE);
+      
+        r.clear_background(Color::WHITE);
+        r.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
     }
+
 }
